efda7cbf9c260b1d1f8d6fefcb96a2fd3f043402
M16yusuf <yusufsmd58@gmail.com>
2025-08-06 06:51:11 +0700

    refactor(navbar-component): change style of navbar component

--- src/components/section/Navbar.jsx
+++ src/components/section/Navbar.jsx
@@ -4,15 +4,15 @@ function Navbar() {
   return (
     <header className="flex flex-row w-full bg-gray-300">
       <nav>
-        <ul>
-          <li>
+        <ul className="flex flex-row p-2.5 gap-2.5">
+          <li className="p-1.5 border rounded-md hover:bg-gray-500 hover:shadow-blue-300 hover:shadow-md">
             <Link to={"/"}>{`e-commerce (State Lifting)`}</Link>
           </li>
-          <li>
+          <li className="p-1.5 border rounded-md hover:bg-gray-500 hover:shadow-blue-300 hover:shadow-md">
             <Link>{`AuthLogin (context)`}</Link>
           </li>
-          <li>
-            <Link>{`to-dp (useReducer)`}</Link>
+          <li className="p-1.5 border rounded-md hover:bg-gray-500 hover:shadow-blue-300 hover:shadow-md">
+            <Link>{`to-doApp (useReducer)`}</Link>
           </li>
         </ul>
       </nav>
